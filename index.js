2f7eb1919bdb7ffb7ea4f10d3346bc46a1b83ac1
Hareem Mohsin <122696027+hareem09@users.noreply.github.com>
2025-03-25 16:35:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+//requiring the module express
 const express=require('express')
 const app=express()
-
+//initialing port
 const port=5000
-
+//doing this so that express can understand json data
 app.use(express.json())
 
+//storing data
 let books=[{
   id:1,
   title:'The Alchemist',
@@ -17,29 +19,36 @@ let books=[{
   year:2019
 }]
 
+//Http methods
+//fetching books
 app.get('/books',(req,res)=>{
   res.json(books )
 })
 
+//using id as params to fetch data according to it
 app.get('/books/:id',(req,res)=>{
    const book=books.find((book)=>book.id==req.params.id)
+   if(!book)
+   return res.status(404).json({message:"Book is not found"})
    res.json(book)
 })
 
+//adding new book 
 app.post('/addbook',(req,res)=>{
-  const {id,title,author,year}=req.body
-  const book={
-  id:id,
-  title:title,
-  author:author,
-  year:year
-  }
-   books.push(book)
+  //destructuring is used
+  const {title,author,year}=req.body
+  if(!title||!author||!year)
+    return res.status(400).json({message:"please enter the details"})
+  const newBook = { id: books.length + 1, title, author, year };
+   books.push(newBook)
   res.json(books)
 })
 
+//updating the data
 app.put('/updatebook/:id',(req,res)=>{
     const book=books.find((book)=>book.id==req.params.id)
+    if(!book)
+      return res.status(404).json({message:"Book not found"})
     book.id=req.body.id
     book.title=req.body.title
     book.author=req.body.author
@@ -48,6 +57,7 @@ app.put('/updatebook/:id',(req,res)=>{
 })
 
 app.delete('/deletebook/:id',(req,res)=>{
+  //id that is not matched will be remain as it is, and that is left is deleted
     books=books.filter((book)=>book.id!=req.params.id)
     res.json(books)
 })
@@ -55,4 +65,5 @@ app.delete('/deletebook/:id',(req,res)=>{
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
   });
+  
   
